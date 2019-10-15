e206ac4f7f885fe8d64cae1d910a6a6ad0bd9e9b
Prudence <prudence@blueanchor.io>
2019-10-15 16:20:00 +0200

    front proxy

--- web-app/src/App.js
+++ web-app/src/App.js
@@ -1,26 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import socketIOClient from 'socket.io-client';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload. SLACKMEHARDER
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+
+  componentDidMount = () => {
+    const socket = socketIOClient('/api/message');
+    socket.emit('change color', this.state.color)
+  }
+
+  testSocket = () => {
+
+
+  }
+  render() {
+    return (
+      <div className="App">
+        <button onClick={this.testSocket}></button>
+      </div>
+    );
+  }
 }
 
 export default App;
