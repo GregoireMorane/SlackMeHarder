255bde2a2be9acd766a5899e907a78bf19ae3e74
wilder <prudence.gaboriau@gmail.com>
2019-10-17 20:00:07 +0200

    display of messages, creation of the input and button to send the message, get of the input value with formdata

--- web-app/src/components/ChatBox/Messages.js
+++ web-app/src/components/ChatBox/Messages.js
@@ -3,15 +3,27 @@ import { useMessages } from './services';
 import './styles.css';
 
 function Messages(props) {
-  const messages = useMessages(props.match.params.id);
-  console.log('messages', messages);
+  const [messages, createMessage] = useMessages(props.match.params.id);
+
+  const onSubmit = async e => {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+    const content = formData.get('content')
+    await createMessage(content);
+  }
+
   return (
     <div className="container__chat">
-      <div className="container__chat--messages">
-        <p>messages</p>
+      <div className="container__chat__messages">
+        {messages && messages.map(message => (
+          <p key={message.id}>{message.content}</p>
+        ))}
       </div>
-      <div className="container__chat--sendBox">
-        <p>input envoi message</p>
+      <div className="container__chat__sendBox">
+        <form onSubmit={e => onSubmit(e)}>
+          <input placeholder="Envoyer un message" name="content"/>
+          <button type="submit">Envoyer</button>        
+        </form>
       </div>
     </div>
   );
