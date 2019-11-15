bcaa24b03f4e8b0ce536d72f5d5fcc533244a2a3
wilder <prudence.gaboriau@gmail.com>
2019-11-15 09:24:55 +0100

    middleware checkSession, save anonymous session

--- api/utils/setSessionId.js
+++ api/utils/setSessionId.js
@@ -1,12 +1,19 @@
-const setSessionId = (req, res, next) => {
+const dataLayer = require('../data-layer');
+
+const setSessionId = async (req, res, next) => {
   const cookie = req.cookies.sessionId;
   if (cookie === undefined) {
     let randomNumber = Math.random().toString();
     sessionId = randomNumber.substring(2, randomNumber.length);
     res.cookie('sessionId', sessionId, { maxAge: 999900000, httpOnly: true });
     console.log('cookie should have been set');
+
+    const sessionExists = await dataLayer.findSessionById(sessionId);
+    if (!sessionExists) {
+      await dataLayer.createSession(sessionId, null);
+    }
   } else {
-    console.log('cookie exist', cookie);
+    console.log('cookie exists', cookie);
   }
   next();
 };
