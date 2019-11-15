31ddb14b0f4dd77d181c48dcad1fa734834d54b3
wilder <prudence.gaboriau@gmail.com>
2019-11-15 09:13:51 +0100

    middleware checkSession, save anonymous session

--- web-app/src/setupProxy.js
+++ web-app/src/setupProxy.js
@@ -1,12 +1,13 @@
 const proxy = require('http-proxy-middleware');
+const PORT = process.env.PORT;
 
 module.exports = function(app) {
   app.use(
-    proxy('/api', { target: 'http://localhost:3001', changeOrigin: true })
+    proxy('/api', { target: `http://localhost:${PORT}`, changeOrigin: true })
   );
   app.use(
     proxy('/socket.io', {
-      target: 'http://localhost:3001',
+      target: `http://localhost:${PORT}`,
       ws: true,
     })
   );
