7f54ce5b140c18bc0a587646a81749e6943ee4bb
GrégoireMorane <gh.morane@gmail.com>
2019-12-10 17:04:17 +0100

    fix(deployment): express loading static only in prod

--- server.js
+++ server.js
@@ -42,15 +42,17 @@ io.on('connection', socket => {
   app.use('/api/channels', routerChannels);
   app.use('/api/messages', routerMessages);
 
-  app.use(express.static(path.join(__dirname, 'web-app', 'build')));
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'web-app', 'build', 'index.html'));
-  });
-
   socket.on('disconnect', function() {
     console.log('user disconnected');
   });
 });
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, 'web-app', 'build')));
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'web-app', 'build', 'index.html'));
+  });
+}
 
 const port = process.env.PORT;
 
