aa0970875d2fa10fd9aff99cbc3fef44c04e9383
Prudence Gaboriau <pgaboriau@imac-de-prudence.home>
2020-01-02 13:12:01 +0100

    replace scrollToBottom animation by a React Ref directly in the component, this way permit to not mock the scrollToBottom func in messageSpec.js, deletion of the animation.js file

--- web-app/src/components/ChatBox/Messages.js
+++ web-app/src/components/ChatBox/Messages.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { useMessages } from './services';
 import {
   isUsernameAndHourNeedToBeDisplayed,
@@ -7,29 +7,35 @@ import {
 import './styles.css';
 
 function Messages(props) {
+  const messageContainerRef = useRef(null);
+
   const {
     messages,
     createMessage,
     contentValue,
     setContentValue,
   } = useMessages(props.match.params.id);
-
-  const _createNewMessage = async e => {
+  
+  const _createNewMessage = async (e, ref) => {
     e.preventDefault();
     await createMessage();
     setContentValue('');
+    await ref.current.scrollIntoView({
+      behavior: 'smooth',
+      block: 'start',
+    });
   };
-
+  
   const _setCurrentMessageContent = e => {
     setContentValue(e.target.value);
   };
-
+  
   return (
     <div className="container__chat">
       <div className="container__chat__messages">
         {messages &&
           messages.map((message, index) => (
-            <div className="container__message" key={message.id}>
+            <div ref={messageContainerRef} className="container__message" key={message.id}>
               {isUsernameAndHourNeedToBeDisplayed(
                 index - 1,
                 message,
@@ -45,7 +51,7 @@ function Messages(props) {
           ))}
       </div>
       <div className="container__chat__sendBox">
-        <form className="form__chat__sendbox" onSubmit={_createNewMessage}>
+        <form className="form__chat__sendbox" onSubmit={(e) => _createNewMessage(e, messageContainerRef)}>
           <input
             className="input__chat__sendbox"
             placeholder="Envoyer un message"
--- web-app/src/utils/animations.js
+++
@@ -1,9 +0,0 @@
-const scrollToBottomOfElement = container => {
-  // scroll to ref instead query selector
-  const element = document.querySelector(container);
-  element.scrollTop = element.scrollHeight;
-};
-
-module.exports = {
-  scrollToBottomOfElement,
-};
